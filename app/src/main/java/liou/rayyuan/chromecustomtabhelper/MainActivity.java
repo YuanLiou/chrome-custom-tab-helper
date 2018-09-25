1519f84f682ec9d7e7122026b2ccda2c24f5a636
Ray-Yuan Liu <louis383@gmail.com>
2018-09-26 00:33:15 +0800

    Fix jitpack issue.

---
+++ app/src/main/java/liou/rayyuan/chromecustomtabhelper/MainActivity.java
@@ -0,0 +1,16 @@
+package liou.rayyuan.chromecustomtabhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by louis383 on 2018/9/26.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
